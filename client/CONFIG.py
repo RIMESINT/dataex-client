ca7ea4053a760d73cb41e5b09a720992623369f2
Anubinda <st120042@ait.asia>
2021-09-08 11:44:07 +0700

    changes to config

--- client/CONFIG.py
+++ client/CONFIG.py
@@ -2,8 +2,8 @@ SECRET_KEY = 'a!2ksd@l68cta%fejtdrv@ac7dy=_06hcyf-@y+ksdy8p%kvs5'
 
 
 
-GET_TOKEN_URL = 'https://dataex.rimes.int/user_auth/get_token/'
-GET_OBS_DATA_URL = 'https://dataex.rimes.int/obs_data/get_obs_data/'
-INSERT_OBS_DATA_URL = 'https://dataex.rimes.int/obs_data/insert_obs_data/'
-GET_NETCDF_SUBSET_URL = 'https://dataex.rimes.int/forecast_sub/get_netcdf_subset/'
-GET_NETCDF_SUBSET_ENS_URL = 'https://dataex.rimes.int/forecast_sub/get_netcdf_subset_ens/'
+GET_TOKEN_URL = 'http://127.0.0.1:8000/user_auth/get_token/'
+GET_OBS_DATA_URL = 'http://127.0.0.1:8000/obs_data/get_obs_data/'
+INSERT_OBS_DATA_URL = 'http://127.0.0.1:8000/obs_data/insert_obs_data/'
+GET_NETCDF_SUBSET_URL = 'http://127.0.0.1:8000/forecast_sub/get_netcdf_subset/'
+GET_NETCDF_SUBSET_ENS_URL = 'http://127.0.0.1:8000/forecast_sub/get_netcdf_subset_ens/'
