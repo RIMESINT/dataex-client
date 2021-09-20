74579a2f8234414de212ab12bb337491c7097852
Anubinda <st120042@ait.asia>
2021-09-20 11:09:32 +0700

    added comma in setup

--- setup.py
+++ setup.py
@@ -14,15 +14,15 @@ setup(
     name = "dataex-client",
     version = "0.1.0",
     python_requires = '>=3.6',
-    author = 'nzahasan, anubinda',
-    author_email = 'nzahasan@gmail.com, grg.jomle@gmail.com',
+    author = 'nzahasan',
+    author_email = 'nzahasan@gmail.com',
     packages = ["client"],
     include_package_data = True,
     url = "https://github.com/nzahasan/dataex-client",
     license = "MIT",
     description = "Dataex-client API for using services by dataex",
-    long_description = readme_contents 
-    scripts =  scripts_list
+    long_description = readme_contents ,
+    scripts =  scripts_list,
     install_requires=[
         "pandas",
         "yaspin",
